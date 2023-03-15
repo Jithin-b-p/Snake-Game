aca77e975d37fae6cf8d401ee7616de22651c744
unknown <jithinbp007@gmail.com>
2023-03-15 14:53:22 +0530

    saving highscore in a txt file

--- scoreboard.py
+++ scoreboard.py
@@ -10,16 +10,26 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.retrieve_high_score()
         self.penup()
         self.color("white")
         self.hideturtle()
         self.goto(0, 270)
         self.show_score()
 
+    def retrieve_high_score(self) -> int:
+        with open("high_score.txt") as file:
+            high_score = int(file.read())
+        return high_score
+
+    def update_high_score(self):
+        with open("high_score.txt", mode="w") as file:
+            file.write(str(self.high_score))
+
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+        self.update_high_score()
         self.score = 0
         self.show_score()
 
